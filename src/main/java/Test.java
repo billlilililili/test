08e30d2cd6ee8475ae2a14be7a6a3357648c1ab8
bill <13922771473@163.com>
2023-10-26 18:12:22 +0800

    hot-fix commit1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,6 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello!");
         System.out.println("Hello Git!");
-        System.out.println("Hello Git3!");
+        System.out.println("Hello Git4!");
     }
 }
